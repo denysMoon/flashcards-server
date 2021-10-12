cabc11ecd09e3ffea5055807131207cc09eadc5f
denysMoon <denysulvanskyi@gmail.com>
2021-10-12 20:08:59 +0300

    edit response

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json())
 app.use(cors())
 
 app.get('/', (req, res) => {
-    res.send('Hi from server!!!')
+    res.send('Hi from server!')
 })
 
 app.use('/flashcards', routerFlashCards)
